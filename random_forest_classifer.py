12aa23200f25e5a0334f1cb0f337db42f03fff7c
duressa@2022 <duressashukuri2022@gmail.com>
2024-09-29 16:01:11 +0300

    random forest classifer is added

--- random_forest_classifer.py
+++ random_forest_classifer.py
@@ -1,10 +1,10 @@
-from decision_regressor import DecisionTreeRegressor
+from advanced_classifer import DecisionTreeClassifer
 import numpy as np
 
-class RandomForestRegressor:
-    def __init__(self,estimators,min_sample_split=2,max_depth=float("inf")):
+class RandomForestClassifer:
+    def __init__(self,estimators=100,min_sample_splits=2,max_depth=float("inf")):
         self.estimators=estimators
-        self.min_sample_split=min_sample_split
+        self.min_sample_splits=min_sample_splits
         self.max_depth=max_depth
         self.trees=[]
 
@@ -16,12 +16,10 @@ class RandomForestRegressor:
     def fit(self,X,y):
         for _ in range(self.estimators):
             X_sample,y_sample=self._random_sample(X,y)
-            tree=DecisionTreeRegressor(min_sample_split=self.min_sample_split,max_depth=self.max_depth)
+            tree=DecisionTreeClassifer(min_sample_split=self.min_sample_splits,max_depth=self.max_depth)
             tree.fit(X_sample,y_sample)
             self.trees.append(tree)
-    
+
     def predict(self,y):
         tree_predictions=np.array([tree.predict(y) for tree in self.trees])
-        return np.mean(tree_predictions,axis=0)
-
-        
+        return np.array([np.argmax(np.bincount(tree_predictions[:,index])) for index in range(tree_predictions.shape[1])])
